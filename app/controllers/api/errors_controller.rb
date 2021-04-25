da794db0c734b78aea8fccb6e55cc0c4ae42cbf7
austin shields <ajshields196@gmail.com>
2021-04-25 11:43:13 -0400

    add range_check to cantus_firmus_error

--- app/controllers/api/errors_controller.rb
+++ app/controllers/api/errors_controller.rb
@@ -1,5 +1,8 @@
 class Api::ErrorsController < ApplicationController
   def create
+    @error = CantusFirmusError.new
+    @notes = params[:notes] || "no notes here"
+    @error.evaluate(@notes)
     render "error.json.jb"
   end
 end
--- app/models/cantus_firmus_error.rb
+++ app/models/cantus_firmus_error.rb
@@ -1,2 +1,30 @@
 class CantusFirmusError < ApplicationRecord
+  attr_accessor :notes, :errors, :suggestions
+
+  def evaluate(input)
+    @notes = input
+    @errors = []
+    @suggestions = []
+    range_check
+
+    if @errors == []
+      @errors << "No errors."
+    end
+    if @suggestions == []
+      @suggestions << "No suggestions."
+    end
+  end
+  
+  def range_check
+    p "range check"
+    if @notes.max - @notes.min > 16
+      p "bad range"
+      @errors << "Range exceeds a 10th."
+    elsif @notes.max - @notes.min > 12
+      p "ehh range"
+      @suggestions << "Range exceeds an octave."
+    else
+      p "good range"
+    end
+  end
 end
